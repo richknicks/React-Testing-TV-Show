146473ed543541c37e0c2f75459d44c8fd5eb992
richknicks <rlynch@gmail.com>
2020-02-25 11:04:55 -0500

    set up app.js

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -5,8 +5,8 @@ import axios from 'axios';
 export const fetchShow = () => {
     return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
       .then(response => {
-          console.log(response.data)
-          return response.data
+          console.log(response)
+          return response
        } )
        .catch(error => console.log('This is an error',error)) 
   }
